8bb31c356c9dc72d732f5a1b1c4534050300f47a
Alexander Hutchinson <ahutchin2003@gmail.com>
2023-01-16 11:27:30 -0500

    implement policy_nn

--- chess_nn.py
+++ chess_nn.py
@@ -8,7 +8,32 @@ class PolicyNN(nn.Module):
 
     def __init__(self):
         super(PolicyNN, self).__init__()
-        pass
+        self.network = nn.Sequential(
+            '''
+            (1) A convolution of 2 filters of kernel size 1x1 with stride 1
+                - I am not sure the difference between convolutional filter and layer
+                - Unclear on inputs/outputs; typically input = batch size
+            (2) Batch normalization
+            (3) A rectifier nonlinearity
+            (4) A fully connected linear layer that outputs a vector of size 19^2 + 1 = 362, corresponding to logit 
+                probabilities for all intersections and the pass move
+                - Unclear on inputs/outputs
+                - I think the second half of this is about the game of GO
+            '''
+
+            # Layer 1
+            nn.Conv2d(in_channels = , out_channels = , kernel_size = 1, stride = 1), 
+            nn.BatchNorm2d(num_features = out_channels),
+            nn.ReLU(),
+
+            # Layer 2
+            nn.Conv2d(in_channels = batch size, out_channels = , kernel_size = 1, stride = 1),
+            nn.BatchNorm2d(num_features = out_channels),
+            nn.ReLU()
+
+            # Fully Connected Layer 
+            nn.Linear(in_features = , out_features = 362) 
+        )
 
     def forward(self, x):
         pass
@@ -38,7 +63,7 @@ class ChessNN(nn.Module):
             First Convolutional Block
             """
             nn.Conv2d(ask thomas, 256, 32, kernel_size = 3, stride = 1),
-            nn.BatchNorm2d(256)
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             
             """
@@ -46,12 +71,12 @@ class ChessNN(nn.Module):
             Make 39 of these residual blocks without repeating
             """
             nn.Conv2d(256, 256, 32, kernel_size = 3, stride = 1),
-            nn.BatchNorm2d(256)
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, 32, kernel_size = 3, stride = 1),
             nn.BatchNorm2d(256),
             ''' Implement skip connection layer'''
-            nn.ReLU(),
+            nn.ReLU()
             
             
         )
